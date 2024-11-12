46683710faa4c31cc79b434f9492129bf44d3871
WCross417 <wcrosby@worcester.edu>
2024-11-12 10:49:49 -0500

    Add files via upload

---
+++ main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Task {
+    private int priorityNum;
+    private String taskData; 
+
+    public Task(int priorityNum, String taskData) {
+        this.priorityNum = priorityNum;
+        this.taskData = taskData;
+    }
+
+    public int getPriority() {
+        
+    }
+}
+
+public static void main(String[] args) {
+    Scanner scnr = new Scanner(System.in);
+    
+
+
+
+
+
+
+}
+
+
+
+
+
