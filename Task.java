9115069b7b9c740f2e6c3c3d5a89ed47ef17e89c
WCross417 <wcrosby@worcester.edu>
2024-11-14 11:13:52 -0500

    Update Task.java

--- Task.java
+++ Task.java
@@ -2,11 +2,13 @@ public class Task {
     private int id;
     private String description;
     private boolean isCompleted; 
+    private String status;
 
     public Task(int id, String description) {
         this.id = id;
         this.description = description;
         this.isCompleted = false;
+        this.status = "In Progress";
     }
 
     public int getId() {
@@ -23,5 +25,10 @@ public class Task {
 
     public void setCompleted(boolean completed) {
         isCompleted = completed;
+        status = "Done";
+    }
+
+    public String toString() {
+        return "Task " + id + ": " + description + "  | " + status + " |";
     }
 }
