116a1b016e7f9eb6e3f381ca37870a998164c9be
WCross417 <wcrosby@worcester.edu>
2024-12-11 19:30:40 -0500

    Update MarkAsDone.java

--- Personal-Planner/src/main/PersonalPlanner/Features/MarkAsDone.java
+++ Personal-Planner/src/main/PersonalPlanner/Features/MarkAsDone.java
@@ -1,5 +1,48 @@
 package main.PersonalPlanner.Features;
 
-public class MarkAsDone {
+import java.util.Scanner;
+
+import main.PersonalPlanner.App.Planner;
+
+public class MarkAsDone extends Actions {
+
+    @Override
+    public void showInfo() {
+        System.out.println("\nTo mark a task done, enter ID");
+        System.out.println("\nEnter 0 to Return to Menu");
+    }
+
+    @Override
+    public String readIn() {
+       while (true) {
+            System.out.println("\nEnter ID: ");
+            Scanner scnr = new Scanner(System.in);
+            try { 
+                String id = scnr.nextLine();
+                if (!id.equals("0")) {
+                    if (Planner.tasks.containsKey(id)) {
+                        return id;
+                    }
+                    else {
+                        System.out.println("ID not found, enter existing ID");
+                    }
+                }
+                else {
+                    return id;
+                }
+                
+            }
+            catch (Exception e) {
+                System.out.println("Enter a valid ID or 0 to return to menu");
+            }
+
+        }
+    }
+
+    @Override
+    public void executeAction(String in) {
+        Planner.tasks.get(in).setStatusDone();
+        System.out.println("Task makred done succesfully!");
+    }
     
 }
