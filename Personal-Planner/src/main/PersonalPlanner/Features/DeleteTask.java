75b17cbac6733606a823123ea20e5566c2078c8f
WCross417 <wcrosby@worcester.edu>
2024-12-11 19:30:01 -0500

    Update DeleteTask.java

--- Personal-Planner/src/main/PersonalPlanner/Features/DeleteTask.java
+++ Personal-Planner/src/main/PersonalPlanner/Features/DeleteTask.java
@@ -1,5 +1,47 @@
 package main.PersonalPlanner.Features;
 
-public class DeleteTask {
+import java.util.Scanner;
+import main.PersonalPlanner.App.*;
+
+public class DeleteTask extends Actions {
+
+    @Override
+    public void showInfo() {
+        System.out.println("\nTo delete a task, enter ID");
+        System.out.println("\nEnter 0 to Return to Menu");
+    }
+
+    @Override
+    public String readIn() {
+        while (true) {
+            System.out.println("\nEnter ID: ");
+            Scanner scnr = new Scanner(System.in);
+            try { 
+                String id = scnr.nextLine();
+                if (!id.equals("0")) {
+                    if (Planner.tasks.containsKey(id)) {
+                        return id;
+                    }
+                    else {
+                        System.out.println("ID not found, enter existing ID");
+                    }
+                }
+                else {
+                    return id;
+                }
+                
+            }
+            catch (Exception e) {
+                System.out.println("Enter a valid ID or 0 to return to menu");
+            }
+
+        }
+    }
+
+    @Override
+    public void executeAction(String in) {
+        Planner.tasks.remove(in);
+        System.out.println("Task removed succesfully!");
+    }
     
 }
