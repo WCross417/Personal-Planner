af1842eed5530f0d937abef659567f5ff16e881e
WCross417 <wcrosby@worcester.edu>
2024-12-11 19:32:07 -0500

    Update ViewTasks.java

--- Personal-Planner/src/main/PersonalPlanner/Features/ViewTasks.java
+++ Personal-Planner/src/main/PersonalPlanner/Features/ViewTasks.java
@@ -1,5 +1,27 @@
 package main.PersonalPlanner.Features;
 
-public class ViewTasks {
+import main.PersonalPlanner.App.*;
+
+public class ViewTasks extends Actions{
+
+    @Override
+    public void showInfo() {
+        throw new UnsupportedOperationException("Unimplemented method 'showInfo'");
+    }
+
+    @Override
+    public String readIn() {
+        throw new UnsupportedOperationException("Unimplemented method 'readIn'");
+    }
+
+    @Override
+    public void executeAction(String in) {
+        System.out.println("| Task | Date (yyyy-MM-dd) | Status | (project name) | ID             |");
+        System.out.println("|---------------------------------------------------------------------|");
+        Planner.tasks.forEach((key, task) -> System.out.println("| " + task.getDescription() 
+        + " |" + task.getDate() + " | " + task.getStatus() + " | " 
+        + "(" + task.getProject() + " )" + " | ID: (" + key + ") ")); 
+        System.out.println("|---------------------------------------------------------------------|");
+    }
     
 }
